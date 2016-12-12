ddf6ff78a8b87097b75282f6cd01a9ab4bda2d4c
mugimudeana <deanamugimu@gmail.com>
2016-12-12 00:06:47 -0600

    com

---
+++ MissionaryWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MissionaryWeb.MvcApplication" Language="C#" %>
---
+++ MissionaryWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MissionaryWeb.Startup))]
+namespace MissionaryWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
